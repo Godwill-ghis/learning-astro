46dc49ce7a729873f4ed7a5b9a081366265afbf0
Godwill <ghisgodwill@gmail.com>
2025-09-26 13:48:40 +0100

    completed asto blog tutorial

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://68d5450ccdae8215112ae7d4--astroblog234.netlify.app",
+  integrations: [preact()]
+});
